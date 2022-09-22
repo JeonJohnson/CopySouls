86f2e6e6450cf2c0babcf661d188ed563e3ac73e
YoungSeok <112372556+han12c36@users.noreply.github.com>
2022-09-22 19:47:35 +0900

    ManagerTest
    매니저 테스트

---
+++ Assets/Scripts/Managers/UnitManager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitManager : Manager<UnitManager>
+{
+    public GameObject playerObj;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
