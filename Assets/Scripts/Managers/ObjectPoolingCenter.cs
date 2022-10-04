397eb1db163722bafa9302b581d3d2e817c83a58
JeonJohnson <78492604+JeonJohnson@users.noreply.github.com>
2022-10-04 11:43:39 +0900

    updater ObjectPoolingCenter and Arcehr Animation
    아바타 마스크 테스트하러 갑니다

--- Assets/Scripts/Managers/ObjectPoolingCenter.cs
+++ Assets/Scripts/Managers/ObjectPoolingCenter.cs
@@ -106,7 +106,7 @@ public class ObjectPoolingCenter : Manager<ObjectPoolingCenter>
 
         CreateBoxes();
         //FindPrefabs();
-        //FillPool();
+        FillObject(ePoolingObj.Arrow);
     }
 
 
