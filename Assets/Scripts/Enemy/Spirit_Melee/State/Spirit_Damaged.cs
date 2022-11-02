e7a59d465b77f52b0900040f05195382fe38d0c4
YoungSeok <112372556+han12c36@users.noreply.github.com>
2022-11-02 11:41:17 +0900

    Update
    Update

---
+++ Assets/Scripts/Enemy/Spirit_Melee/State/Spirit_Damaged.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spirit_Damaged : cState
+{
+    public override void EnterState(Enemy script)
+    {
+        base.EnterState(script);
+    }
+
+    public override void UpdateState()
+    {
+       
+    }
+
+    public override void ExitState()
+    {
+        
+    }
+}
