b18286eed402176615cce61a51ee997e9a8ef32d
YoungSeok <112372556+han12c36@users.noreply.github.com>
2022-11-16 11:34:24 +0900

    Update
    Update

--- Assets/Scripts/Enemy/Spirit_Melee/State/Spirit_Return.cs
+++ Assets/Scripts/Enemy/Spirit_Melee/State/Spirit_Return.cs
@@ -4,49 +4,48 @@ using UnityEngine;
 
 public class Spirit_Return : cState
 {
-    public bool Complete_Search;
+    public bool Complete_Schouting;
 
     public override void EnterState(Enemy script)
     {
         base.EnterState(script);
         me.MoveStop();
         me.animCtrl.SetBool("isReturn", true);
-        me.animCtrl.SetBool("isUnequipt", true);
+        me.GetComponent<FieldOfView>().viewAngle = ((Spirit)me).initFOVAngle;
     }
 
     public override void UpdateState()
     {
         //if (me.combatState == eCombatState.Alert) me.SetState((int)Enums.eSpiritState.Equipt);
 
-        if (me.animCtrl.GetCurrentAnimatorStateInfo(0).IsName("UnEquipt"))
+        //함 쨰리보고 본래 자리로 돌아가서 idle
+
+        if (me.animCtrl.GetCurrentAnimatorStateInfo(0).IsName("ReturnStart"))
         {
             if (((Spirit)me).isCurrentAnimationOver(me.animCtrl, 1f))
             {
-                if (me.animCtrl.GetBool("isUnequipt"))
+                if (!me.animCtrl.GetBool("isSchouting"))
                 {
-                    me.animCtrl.SetBool("isUnequipt", false);
-                    me.GetComponent<FieldOfView>().viewAngle = ((Spirit)me).initFOVAngle;
-                    Complete_Search = true;
+                    me.animCtrl.SetBool("isSchouting", true);
+                    Complete_Schouting = true;
                 }
             }
         }
 
-        if(Complete_Search)
+        if(Complete_Schouting)
         {
             if(Vector3.Distance(me.transform.position,((Spirit)me).respawnPos) > 0.5f)
             {
+                Debug.Log("도착 못함");
                 me.MoveOrder(((Spirit)me).respawnPos);
-                me.animCtrl.SetBool("isWalk", true);
-                me.animCtrl.SetBool("isPatrol", true);
             }
-            else
+            else if(Vector3.Distance(me.transform.position, ((Spirit)me).respawnPos) <= 0.5f || me.transform.position == ((Spirit)me).respawnPos)
             {
                 Debug.Log("도착!!!!!!!!!");
                 me.MoveStop();
+                me.animCtrl.SetBool("isReturn", false);
                 ((Spirit)me).isReturn = false;
-                me.animCtrl.SetBool("isWalk", false);
-                me.animCtrl.SetBool("isPatrol", false);
-                Complete_Search = false;
+                Complete_Schouting = false;
                 me.SetState((int)Enums.eSpiritState.Idle);
             }
         }
@@ -54,9 +53,7 @@ public class Spirit_Return : cState
     public override void ExitState()
     {
         me.animCtrl.SetBool("isReturn", false);
-        me.animCtrl.SetBool("isWalk", false);
-        me.animCtrl.SetBool("isPatrol", false);
-        Complete_Search = false;
+        Complete_Schouting = false;
     }
 
 
