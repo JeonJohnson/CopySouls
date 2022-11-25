498abdea67dfd8a657a33b60197c15c96c9a4e34
finalgod5 <113405382+finalgod5@users.noreply.github.com>
2022-11-25 20:17:58 +0900

    Update

---
+++ Assets/Scripts/IntroSceneManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class IntroSceneManager : Manager<IntroSceneManager>
+{
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
