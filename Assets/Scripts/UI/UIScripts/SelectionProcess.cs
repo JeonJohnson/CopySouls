be06fed32701749a283c45ba13832e71e8f26dbe
Jeongmin Yoon <44731636+PizzaEtg@users.noreply.github.com>
2022-11-28 20:08:36 +0900

    fix player weapon null error
    이제 인벤토리는 옵젝풀링 매니저의 프리팹에서 값을 불러와서 플레이어손에 던져줌

--- Assets/Scripts/UI/UIScripts/SelectionProcess.cs
+++ Assets/Scripts/UI/UIScripts/SelectionProcess.cs
@@ -192,9 +192,9 @@ public class SelectionProcess : MonoBehaviour
             EquiptSlot.AddEquiptment(_curSlot, _curSlot.item, 1, EquiptSlot);
         }
         else Debug.Log("EquiptSlot == null");
-
         if (_curSlot.item.GetComponent<Player_Weapon>().type == eWeaponType.Melee)
         {
+            print(_curSlot.item.name + " " + _curSlot.item.gameObject);
             _curSlot.item.GetComponent<Item_Weapon>().SetAsMainWeapon();
         }
         else if (_curSlot.item.GetComponent<Player_Weapon>().type == eWeaponType.Sheild)
