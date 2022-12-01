fca065acb70abc8d66796ba27a9dd3934a774e84
han12c36 <112372556+han12c36@users.noreply.github.com>
2022-12-01 16:33:54 +0900

    Update
    Update

--- Assets/Scripts/CameraShake/New Folder/CameraEffect.cs
+++ Assets/Scripts/CameraShake/New Folder/CameraEffect.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class CameraEffect : MonoBehaviour
 {
     public static string EffectDataPrfabFolderPath = "EffectDataPrefabs";
@@ -53,7 +54,7 @@ public class CameraEffect : MonoBehaviour
             EffectData data = Dic_EffectDatas[dataName];
             //List_EffectDatas.Add(data);
 
-            curData = SelectCurData(data);
+            curData = data;//SelectCurData(data);
             Debug.Log(curData.name + "을 쉐이크 할꺼임");
         }
         else Debug.Log("해당 데이터는 딕셔너리에 존재하지 않습니다.");
@@ -62,89 +63,89 @@ public class CameraEffect : MonoBehaviour
     //기본적으로 이미 실행한 것들은 모두 수행하네
     //그럼 끝날때 인자 하나 받아서 끝났다는 체크 해야겠네
 
-    private EffectData SelectCurData(EffectData data)
-    {
-        //if (List_EffectDatas.Capacity == 0) return null;
-        if (curData == null)
-        {
-            if (!data.GetStart) data.GetStart = true;
-            return data;
-        }
-        if (curData == data)
-        {
-            //실행중 똑같은거 드오면 드온놈 실행
-            Debug.Log("똑같은 쉐이크 시간연장!!!!!!");
-            curData.GetStart = false;
-            if (!data.GetStart) data.GetStart = true;
-            return data;
-        }
-
-        if (curData.duration == data.duration)
-        {
-            if (curData.GetScore >= data.GetScore)
-            {
-                if (!curData.GetStart) curData.GetStart = true;
-                return curData;
-            }
-            else
-            {
-                if (!data.GetStart) data.GetStart = true;
-                return data;
-            }
-        }
-        else if (curData.duration > data.duration)
-        {
-            if (curData.GetScore > data.GetScore)
-            {
-                //data 씹힘
-                Debug.Log("금방 누른 쉐이크 씹힘!!!!!!");
-                if (!curData.GetStart) curData.GetStart = true;
-                return curData;
-            }
-            else
-            {
-                //<중첩되는 시간동안 강도 높여줌>
-                Debug.Log("지금 잠깐 강도 올라감!!!!!!");
-
-                curData.Conflict = true;        //이 시점부터
-                curData.AddValue = 2f;  //이만큼의 벡터를 더해줄꺼임
-                curData.ConflictTime = curData.CurrentTime + data.duration;
-                return curData;
-            }
-        }
-        else if (curData.duration < data.duration)
-        {
-            if (curData.GetScore > data.GetScore)
-            {
-                //중첩시간동안 씹히다가
-                //중첩시간 끝나면 다시 본래의 강도로 돌아옴
-
-                if (!curData.GetStart)
-                {
-                    Debug.Log("씹히다가 원래 지금 드온 강도로 변환!!!!!!");
-
-                    data.CurrentTime = data.duration - (curData.duration - curData.CurrentTime);
-                    if (!data.GetStart) data.GetStart = true;
-                    return data;
-                }
-            }
-            else
-            {
-                //중첩되는 시간동안 강도 높여줌
-                //중첩시간 끝나면 다시 본래의 강도로 돌아옴
-
-                curData.Conflict = true;        //이 시점부터
-                curData.AddValue = 2f;  //이만큼의 벡터를 더해줄꺼임
-                if (!curData.GetStart)
-                {
-                    Debug.Log("강도 높아졌다가 지금 드온 강도로 변환!!!!!!");
-
-                    data.CurrentTime = data.duration - (curData.duration - curData.CurrentTime);
-                    return data;
-                }
-            }
-        }
-        
-        return null;
-    }
+    //private EffectData SelectCurData(EffectData data)
+    //{
+    //    //if (List_EffectDatas.Capacity == 0) return null;
+    //    if (curData == null)
+    //    {
+    //        if (!data.GetStart) data.GetStart = true;
+    //        return data;
+    //    }
+    //    if (curData == data)
+    //    {
+    //        //실행중 똑같은거 드오면 드온놈 실행
+    //        Debug.Log("똑같은 쉐이크 시간연장!!!!!!");
+    //        curData.GetStart = false;
+    //        if (!data.GetStart) data.GetStart = true;
+    //        return data;
+    //    }
+    //
+    //    if (curData.duration == data.duration)
+    //    {
+    //        if (curData.GetScore >= data.GetScore)
+    //        {
+    //            if (!curData.GetStart) curData.GetStart = true;
+    //            return curData;
+    //        }
+    //        else
+    //        {
+    //            if (!data.GetStart) data.GetStart = true;
+    //            return data;
+    //        }
+    //    }
+    //    else if (curData.duration > data.duration)
+    //    {
+    //        if (curData.GetScore > data.GetScore)
+    //        {
+    //            //data 씹힘
+    //            Debug.Log("금방 누른 쉐이크 씹힘!!!!!!");
+    //            if (!curData.GetStart) curData.GetStart = true;
+    //            return curData;
+    //        }
+    //        else
+    //        {
+    //            //<중첩되는 시간동안 강도 높여줌>
+    //            Debug.Log("지금 잠깐 강도 올라감!!!!!!");
+    //
+    //            curData.Conflict = true;        //이 시점부터
+    //            curData.AddValue = 2f;  //이만큼의 벡터를 더해줄꺼임
+    //            curData.ConflictTime = curData.CurrentTime + data.duration;
+    //            return curData;
+    //        }
+    //    }
+    //    else if (curData.duration < data.duration)
+    //    {
+    //        if (curData.GetScore > data.GetScore)
+    //        {
+    //            //중첩시간동안 씹히다가
+    //            //중첩시간 끝나면 다시 본래의 강도로 돌아옴
+    //
+    //            if (!curData.GetStart)
+    //            {
+    //                Debug.Log("씹히다가 원래 지금 드온 강도로 변환!!!!!!");
+    //
+    //                data.CurrentTime = data.duration - (curData.duration - curData.CurrentTime);
+    //                if (!data.GetStart) data.GetStart = true;
+    //                return data;
+    //            }
+    //        }
+    //        else
+    //        {
+    //            //중첩되는 시간동안 강도 높여줌
+    //            //중첩시간 끝나면 다시 본래의 강도로 돌아옴
+    //
+    //            curData.Conflict = true;        //이 시점부터
+    //            curData.AddValue = 2f;  //이만큼의 벡터를 더해줄꺼임
+    //            if (!curData.GetStart)
+    //            {
+    //                Debug.Log("강도 높아졌다가 지금 드온 강도로 변환!!!!!!");
+    //
+    //                data.CurrentTime = data.duration - (curData.duration - curData.CurrentTime);
+    //                return data;
+    //            }
+    //        }
+    //    }
+    //    
+    //    return null;
+    //}
 }
--- Assets/Scripts/CameraShake/New Folder/EffectData.cs
+++ Assets/Scripts/CameraShake/New Folder/EffectData.cs
@@ -2,180 +2,339 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
 [CreateAssetMenu(fileName = "Shake Data", menuName = "Camera Effect Data/Shake Data", order = 1)]
 public class EffectData : ScriptableObject
 {
-    bool isOver;
-    bool isConflict;
-    bool isStart;
-    bool calculate;
-    float addValue;
-    float conflictTime = 0.0f;
+    //public const float COEF_SHAKE_POSITION = 01.0f;
+    //public const float COEF_SHAKE_ROTATION = 25.0f;
 
-    float currentTime = 0.0f;
+    private const float SEED_MIN = 0.0f;
+    private const float SEED_MAX = 1000.0f;
 
-
-    [Header("Value")]
-    public float duration;      //지속력              //
-    [Range(0, 10)]
-    public float amplitude;     //강도                //
-
-    public bool Quick;
-    [Range(0, 5)]
-    public int frequency;     //흔들리는 빈도       // SCORE
-
-    [Header("Transform Value")]
-    [Range(0, 10)]
-    public float Transform_X;
-    [Range(0, 10)]
-    public float Transform_Y;
-    [Range(0, 10)]
-    public float Transform_Z;
-
-    public float Transform_Amplitude;
+    public AnimationCurve Curve = AnimationCurve.EaseInOut(
+            0.0f,
+            1.0f,
+            1.0f,
+            0.0f
+        );
+    // PROPERTIES: ----------------------------------------------------------------------------
+    public bool shakePosition = true;
+    public bool shakeRotation = true;
+    public float duration;
+    public float magnitude;
+    public float roughness;
+    public float perlinSpeed;
+    public float radius;
+    
 
 
-    [Header("Rotation Value")]
-    [Range(0, 10)]
-    public float Rotation_X;
-    [Range(0, 10)]
-    public float Rotation_Y;
-    [Range(0, 10)]
-    public float Rotation_Z;
+    private Vector3 originPos;
+    private Vector3 originRot;
+    private Vector3 seed;
+    
+    private float startTime;
+    public float currentTime;
 
-    public float Rotation_Amplitude;
-
-    float Tr_x;
-    float Tr_y;
-    float Tr_z;
-
-    float Rot_x;
-    float Rot_y;
-    float Rot_z;
-
-    float Score;
-    public float GetScore {get { return Score; }}
-    public bool GetStart { get { return isStart; } set { isStart = value; } }
-    public bool Conflict { get { return isConflict; } set { isConflict = value; } }
-    public float AddValue { get { return addValue; } set { addValue = value; } }
-    public float ConflictTime { get { return conflictTime; } set { conflictTime = value; } }
-    public float CurrentTime { get { return currentTime; } set { currentTime = value; } }
-    public bool Over { get { return isOver; } set { isOver = value; } }
-
-
-
-
-    public EffectData(float _duration,float _amplitude, int _frequency,
-        float _Transform_X, float _Transform_Y,float _Transform_Z,float _Transform_Amplitude,
-        float _Rotation_X, float _Rotation_Y, float _Rotation_Z, float _Rotation_Amplitude)
+    // INITIALIZERS: --------------------------------------------------------------------------
+    private void Initialize()
     {
-        duration = _duration;
-        amplitude = _amplitude;
-        frequency = _frequency;
-        Transform_X = _Transform_X;
-        Transform_Y = _Transform_Y;
-        Transform_Z = _Transform_Z;
-        Transform_Amplitude = _Transform_Amplitude;
-        Rotation_X = _Rotation_X;
-        Rotation_Y = _Rotation_Y;
-        Rotation_Z = _Rotation_Z;
-        Rotation_Amplitude = _Rotation_Amplitude;
+        originPos =  Camera.main.transform.localPosition;
+        originRot = Camera.main.transform.localEulerAngles;
+
+        //this.magnitude = 1.0f;
+        //this.roughness = 1.0f;
+        //this.perlinSpeed = 0.0f;
+        
+        this.seed = new Vector3(
+            Random.Range(SEED_MIN, SEED_MAX),
+            Random.Range(SEED_MIN, SEED_MAX),
+            Random.Range(SEED_MIN, SEED_MAX)
+        );
+
+        this.startTime = Time.time;
+        //this.duration = 1.0f;
+    }
+    public EffectData(float duration, float roughness, float magnitude,
+        bool shakePosition, bool shakeRotation,float radius, AnimationCurve curve)
+    {
+        this.Initialize();
+
+        this.shakePosition = shakePosition;
+        this.shakeRotation = shakeRotation;
+        this.duration = duration;
+        this.roughness = roughness;
+        this.magnitude = magnitude;
+        this.radius = radius;
+        this.Curve = curve;
+    }
+    public EffectData(float duration, EffectData cameraShake)
+    {
+        this.startTime = Time.time;
+        this.seed = cameraShake.seed;
+        this.perlinSpeed = cameraShake.perlinSpeed;
+
+        this.shakePosition = cameraShake.shakePosition;
+        this.shakeRotation = cameraShake.shakeRotation;
+
+        this.duration = duration;
+        this.roughness = cameraShake.roughness;
+        this.magnitude = cameraShake.magnitude;
+
+        this.originPos = cameraShake.originPos;
+        this.originRot = cameraShake.originRot;
+        this.radius = cameraShake.radius;
     }
 
-    public void Start()
-    {
-        Debug.Log("언제 들어오는지 확인하자~~~");
-    }
     public void Update()
     {
-        if(isStart)
+        Debug.Log(duration);
+        if(currentTime >= duration)
         {
-            if (currentTime < duration)
-            {
-                if (!calculate) Calculation();
-                currentTime += Time.deltaTime;
-                Shake();
-            }
-            else
-            {
-                Camera.main.transform.localPosition = CameraEffect.instance.OriginPos;
-                Camera.main.transform.localEulerAngles = CameraEffect.instance.OriginRot;
-                CameraEffect.instance.curData = null;
-                calculate = false;
-                isStart = false;
-                addValue = 1;
-                conflictTime = 0.0f;
-                currentTime = 0.0f;
-            }
+            currentTime = 0.0f;
+            perlinSpeed = 0.0f;
+            originPos = Vector3.zero;
+            originRot = Vector3.zero;
+            CameraEffect.instance.curData = null;
         }
-    }
-
-    private void Calculation()
-    {
-        calculate = true;
-        Tr_x = Transform_X * Transform_Amplitude;
-        Tr_y = Transform_Y * Transform_Amplitude;
-        Tr_z = Transform_Z * Transform_Amplitude;
-
-        Rot_x = Rotation_X * Rotation_Amplitude;
-        Rot_y = Rotation_Y * Rotation_Amplitude;
-        Rot_z = Rotation_Z * Rotation_Amplitude;
-
-        Score = Transform_X;
+        else
+        {
+            currentTime += Time.deltaTime;
+            Shake();
+        }
+        //return amount * this.magnitude * coefficient;
     }
 
     private void Shake()
     {
-        Debug.Log("흔드는 중~~");
-        if (Quick)
-        {
-            Vector3 vector;
-            for (int i = 0; i < frequency; i++)
-            {
-                Vector3 randVec = Random.onUnitSphere * amplitude;
-                randVec.x *= Tr_x;
-                randVec.y *= Tr_y;
-                randVec.z *= Tr_z;
-                randVec = new Vector3(randVec.x, randVec.y, randVec.z);
+        Vector3 amount = new Vector3(
+            Mathf.PerlinNoise(this.perlinSpeed, this.seed.x) - 0.5f,
+            Mathf.PerlinNoise(this.perlinSpeed, this.seed.y) - 0.5f,
+            Mathf.PerlinNoise(this.perlinSpeed, this.seed.z) - 0.5f
+        );
 
-                Vector3 randRot = Random.onUnitSphere * amplitude;
-                randRot.x *= Rot_x;
-                randRot.y *= Rot_y;
-                randRot.z *= Rot_z;
+        this.perlinSpeed += Time.deltaTime * this.roughness;
 
-                if (Conflict)
-                {
-                    randVec *= addValue;
-                    if (currentTime >= conflictTime) Conflict = false;
-                }
+        float coefficient = 1.0f;
 
-                Camera.main.transform.localPosition = CameraEffect.instance.OriginPos + randVec;
-                Camera.main.transform.localEulerAngles = CameraEffect.instance.OriginRot + randRot;
-            }
-        }
-        else
-        {
-            Vector3 randVec = Random.onUnitSphere * amplitude;
-            randVec.x *= Tr_x;
-            randVec.y *= Tr_y;
-            randVec.z *= Tr_z;
-            randVec = new Vector3(randVec.x, randVec.y, randVec.z);
-
-            Vector3 randRot = Random.onUnitSphere * amplitude;
-            randRot.x *= Rot_x;
-            randRot.y *= Rot_y;
-            randRot.z *= Rot_z;
-
-            if (Conflict)
-            {
-                randVec *= addValue;
-                if (currentTime >= conflictTime) Conflict = false;
-            }
-
-            Camera.main.transform.localPosition = CameraEffect.instance.OriginPos + randVec;
-            Camera.main.transform.localEulerAngles = CameraEffect.instance.OriginRot + randRot;
-        }
+        //float distance = Vector3.Distance(this.originPos, Camera.main.transform.localPosition);
+        //
+        //Debug.Log(Mathf.Clamp01(distance / this.radius));
+        //
+        //coefficient = 1f - Mathf.Clamp01(distance / this.radius);
+        //
+        //Debug.Log("양 : " +amount + "컨피시언트 : " + coefficient);
+        
+        if(shakePosition) Camera.main.transform.localPosition = CameraEffect.instance.OriginPos + amount * this.magnitude * coefficient;
+        if(shakeRotation) Camera.main.transform.localEulerAngles = CameraEffect.instance.OriginRot + amount * this.magnitude * coefficient;
+        //Camera.main.transform.localPosition += amount * this.magnitude * coefficient;
+        //Camera.main.transform.localPosition += amount * this.magnitude * coefficient;
     }
+
+    //public float GetEasing()
+    //{
+    //    //return EASING.Evaluate(this.GetNormalizedProgress());
+    //}
+
+    public bool IsComplete()
+    {
+        return this.GetNormalizedProgress() >= 1.0f;
+    }
+
+    public Vector3 GetInfluencePosition()
+    {
+        return (this.shakePosition ? Vector3.one : Vector3.zero);
+    }
+
+    public Vector3 GetInfluenceRotation()
+    {
+        return (this.shakeRotation ? Vector3.one : Vector3.zero);
+    }
+
+    // PRIVATE METHODS: -----------------------------------------------------------------------
+
+    private float GetNormalizedProgress()
+    {
+        return Mathf.Clamp01((Time.time - this.startTime) / this.duration);
+    }
+}
+//bool isOver;
+//bool isConflict;
+//bool isStart;
+//bool calculate;
+//float addValue;
+//float conflictTime = 0.0f;
+//
+//float currentTime = 0.0f;
+
+
+//[Header("Value")]
+//public float duration;      //지속력              //
+//[Range(0, 10)]
+//public float amplitude;     //강도                //
+//
+//public bool Quick;
+//[Range(0, 5)]
+//public int frequency;     //흔들리는 빈도       // SCORE
+//
+//[Header("Transform Value")]
+//[Range(0, 10)]
+//public float Transform_X;
+//[Range(0, 10)]
+//public float Transform_Y;
+//[Range(0, 10)]
+//public float Transform_Z;
+//
+//public float Transform_Amplitude;
+//
+//
+//[Header("Rotation Value")]
+//[Range(0, 10)]
+//public float Rotation_X;
+//[Range(0, 10)]
+//public float Rotation_Y;
+//[Range(0, 10)]
+//public float Rotation_Z;
+//
+//public float Rotation_Amplitude;
+//
+//float Tr_x;
+//float Tr_y;
+//float Tr_z;
+//
+//float Rot_x;
+//float Rot_y;
+//float Rot_z;
+//
+//float Score;
+//
+//[SerializeField]
+//private AnimationCurve curve;
+
+//public float GetScore {get { return Score; }}
+//public bool GetStart { get { return isStart; } set { isStart = value; } }
+//public bool Conflict { get { return isConflict; } set { isConflict = value; } }
+//public float AddValue { get { return addValue; } set { addValue = value; } }
+//public float ConflictTime { get { return conflictTime; } set { conflictTime = value; } }
+//public float CurrentTime { get { return currentTime; } set { currentTime = value; } }
+//public bool Over { get { return isOver; } set { isOver = value; } }
+
+
+
+
+//public EffectData(float _duration,float _amplitude, int _frequency,
+//    float _Transform_X, float _Transform_Y,float _Transform_Z,float _Transform_Amplitude,
+//    float _Rotation_X, float _Rotation_Y, float _Rotation_Z, float _Rotation_Amplitude)
+//{
+//    duration = _duration;
+//    amplitude = _amplitude;
+//    frequency = _frequency;
+//    Transform_X = _Transform_X;
+//    Transform_Y = _Transform_Y;
+//    Transform_Z = _Transform_Z;
+//    Transform_Amplitude = _Transform_Amplitude;
+//    Rotation_X = _Rotation_X;
+//    Rotation_Y = _Rotation_Y;
+//    Rotation_Z = _Rotation_Z;
+//    Rotation_Amplitude = _Rotation_Amplitude;
+//}
+
+//public void Start()
+//    {
+//        Debug.Log("언제 들어오는지 확인하자~~~");
+//    }
+//   public void Update()
+//  {
+
+
+        //if(isStart)
+        //{
+        //    if (currentTime < duration)
+        //    {
+        //        if (!calculate) Calculation();
+        //        currentTime += Time.deltaTime;
+        //        Shake();
+        //    }
+        //    else
+        //    {
+        //        Camera.main.transform.localPosition = CameraEffect.instance.OriginPos;
+        //        Camera.main.transform.localEulerAngles = CameraEffect.instance.OriginRot;
+        //        CameraEffect.instance.curData = null;
+        //        calculate = false;
+        //        isStart = false;
+        //        addValue = 1;
+        //        conflictTime = 0.0f;
+        //        currentTime = 0.0f;
+        //    }
+        //}
+//    }
+
+    //private void Calculation()
+    //{
+    //    calculate = true;
+    //    Tr_x = Transform_X * Transform_Amplitude;
+    //    Tr_y = Transform_Y * Transform_Amplitude;
+    //    Tr_z = Transform_Z * Transform_Amplitude;
+    //
+    //    Rot_x = Rotation_X * Rotation_Amplitude;
+    //    Rot_y = Rotation_Y * Rotation_Amplitude;
+    //    Rot_z = Rotation_Z * Rotation_Amplitude;
+    //
+    //    Score = Transform_X;
+    //}
+
+    //private void Shake()
+    //{
+        //Debug.Log(curve.Evaluate(currentTime));
+
+        //if (Quick)
+        //{
+        //    Vector3 vector;
+        //    for (int i = 0; i < frequency; i++)
+        //    {
+        //        Vector3 randVec = Random.onUnitSphere * amplitude;
+        //        randVec.x *= Tr_x;
+        //        randVec.y *= Tr_y;
+        //        randVec.z *= Tr_z;
+        //        randVec = new Vector3(randVec.x, randVec.y, randVec.z);
+        //
+        //        Vector3 randRot = Random.onUnitSphere * amplitude;
+        //        randRot.x *= Rot_x;
+        //        randRot.y *= Rot_y;
+        //        randRot.z *= Rot_z;
+        //
+        //        if (Conflict)
+        //        {
+        //            randVec *= addValue;
+        //            if (currentTime >= conflictTime) Conflict = false;
+        //        }
+        //
+        //        Camera.main.transform.localPosition = CameraEffect.instance.OriginPos + randVec;
+        //        Camera.main.transform.localEulerAngles = CameraEffect.instance.OriginRot + randRot;
+        //    }
+        //}
+        //Vector3 randVec = Random.onUnitSphere * amplitude;
+        //
+        //randVec.x *= Tr_x;
+        //randVec.y *= Tr_y;
+        //randVec.z *= Tr_z;
+        //randVec = new Vector3(randVec.x, randVec.y, randVec.z);
+        //
+        //Vector3 randRot = Random.onUnitSphere * amplitude;
+        //randRot.x *= Rot_x;
+        //randRot.y *= Rot_y;
+        //randRot.z *= Rot_z;
+        //
+        //if (Conflict)
+        //{
+        //    randVec *= addValue;
+        //    if (currentTime >= conflictTime) Conflict = false;
+        //}
+        //
+        //Camera.main.transform.localPosition = CameraEffect.instance.OriginPos + randVec;
+        //Camera.main.transform.localEulerAngles = CameraEffect.instance.OriginRot + randRot;
+    //}
 
     //private void HandleCameraCollisions()
     //{
@@ -198,6 +357,7 @@ public class EffectData : ScriptableObject
     //    cameraVectorPosition.z = Mathf.Lerp(cameraTransform.localPosition.z, targetPosition, 0.2f);
     //    cameraTransform.localPosition = cameraVectorPosition;
     //}
+    
 
 
-}
+
